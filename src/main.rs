150ea9fed49796f2843de78f0287a3de76fd79fb
xenozoid <xenozoid@gmail.com>
2018-03-01 14:33:08 +0300

    bump version

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ fn main() {
 fn usage_info(t: &mut Box<StdoutTerminal>) {
     print!("FSelect utility v");
     t.fg(term::color::BRIGHT_YELLOW).unwrap();
-    println!("0.2.0");
+    println!("0.3.0");
     t.reset().unwrap();
 
     println!("Find files with SQL-like queries.");

18c74666d6d2969cba4450c6cc2056efa9789cb2
xenozoid <xenozoid@gmail.com>
2019-01-12 17:03:41 +0300

    update list of file extensions for is_source column

--- src/searcher.rs
+++ src/searcher.rs
@@ -2522,7 +2522,7 @@ def_extension_queries! {
 ;   is_doc                  [".accdb", ".doc", ".docm", ".docx", ".dot", ".dotm", ".dotx", ".mdb", ".ods", ".odt", ".pdf", ".potm", ".potx", ".ppt", ".pptm", ".pptx", ".rtf", ".xlm", ".xls", ".xlsm", ".xlsx", ".xlt", ".xltm", ".xltx", ".xps"]
 ;   is_image                [".bmp", ".gif", ".jpeg", ".jpg", ".png", ".tiff", ".webp"]
 ;   is_image_dim_readable   [".bmp", ".gif", ".jpeg", ".jpg", ".png", ".webp"]
-;   is_source               [".asm", ".c", ".cc", ".ceylon", ".clj", ".cpp", ".cs", ".dart", ".elm", ".erl", ".go", ".groovy", ".h", ".hpp", ".java", ".js", ".jsp", ".kt", ".lua", ".nim", ".pas", ".php", ".pl", ".pm", ".py", ".rb", ".rs", ".scala", ".swift", ".tcl", ".vala"]
+;   is_source               [".asm", ".bas", ".c", ".cc", ".ceylon", ".clj", ".coffee", ".cpp", ".cs", ".dart", ".elm", ".erl", ".go", ".groovy", ".h", ".hpp", ".java", ".js", ".jsp", ".kt", ".kts", ".lua", ".nim", ".pas", ".php", ".pl", ".pm", ".py", ".rb", ".rs", ".scala", ".swift", ".tcl", ".vala", ".vb"]
 ;   is_video                [".3gp", ".avi", ".flv", ".m4p", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".webm", ".wmv"]
 }
 

3ec1d899d393040fc2b5f090b5b43909fe4c2f56
xenozoid <xenozoid@gmail.com>
2019-12-22 14:49:55 +0300

    improve Japanese symbols detection in file names

--- src/util/japanese.rs
+++ src/util/japanese.rs
@@ -1,36 +1,19 @@
 pub fn contains_japanese(s: &str) -> bool {
-    let tokens = wana_kana::tokenize::tokenize(s);
-    let result = tokens.iter().any(|token|
-        wana_kana::is_kana::is_kana(token) | wana_kana::is_kanji::is_kanji(token)
-    );
-
-    return result;
+    s.chars().any(|c| wana_kana::utils::is_char_japanese(c))
 }
 
 pub fn contains_hiragana(s: &str) -> bool {
-    let tokens = wana_kana::tokenize::tokenize(s);
-    let result = tokens.iter().any(|token| wana_kana::is_hiragana::is_hiragana(token));
-
-    return result;
+    s.chars().any(|c| wana_kana::utils::is_char_hiragana(c))
 }
 
 pub fn contains_katakana(s: &str) -> bool {
-    let tokens = wana_kana::tokenize::tokenize(s);
-    let result = tokens.iter().any(|token| wana_kana::is_katakana::is_katakana(token));
-
-    return result;
+    s.chars().any(|c| wana_kana::utils::is_char_katakana(c))
 }
 
 pub fn contains_kana(s: &str) -> bool {
-    let tokens = wana_kana::tokenize::tokenize(s);
-    let result = tokens.iter().any(|token| wana_kana::is_kana::is_kana(token));
-
-    return result;
+    s.chars().any(|c| wana_kana::utils::is_char_kana(c))
 }
 
 pub fn contains_kanji(s: &str) -> bool {
-    let tokens = wana_kana::tokenize::tokenize(s);
-    let result = tokens.iter().any(|token| wana_kana::is_kanji::is_kanji(token));
-
-    return result;
+    s.chars().any(|c| wana_kana::utils::is_char_kanji(c))
 }

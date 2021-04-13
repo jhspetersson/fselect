d6cba5bd7863beb1b3d8348d43c8d2d5bdf1072f
jhspetersson <jhspetersson@gmail.com>
2021-04-13 19:03:19 +0300

    Interactive mode not working #99

--- src/main.rs
+++ src/main.rs
@@ -107,9 +107,13 @@ fn main() {
         args.remove(0);
 
         if args.is_empty() {
-            short_usage_info(no_color);
-            help_hint();
-            return;
+            if !interactive {
+                short_usage_info(no_color);
+                help_hint();
+                return;
+            } else {
+                break;
+            }
         }
 
         first_arg = args[0].to_ascii_lowercase();

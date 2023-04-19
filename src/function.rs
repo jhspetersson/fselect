f19a424f52d38ecde3d645431d164b6760c4e544
jhspetersson <jhspetersson@gmail.com>
2023-04-19 09:48:04 +0200

    replace panic with error handling during parsing datetime #130

--- src/function.rs
+++ src/function.rs
@@ -17,7 +17,7 @@ use serde::ser::{Serialize, Serializer};
 use xattr::FileExt;
 
 use crate::fileinfo::FileInfo;
-use crate::util::{capitalize, format_date};
+use crate::util::{capitalize, error_message, format_date};
 use crate::util::format_datetime;
 use crate::util::parse_datetime;
 use crate::util::parse_filesize;
@@ -206,7 +206,10 @@ impl Variant {
                 Ok((dt_from, dt_to)) => {
                     return (dt_from, dt_to);
                 },
-                _ => panic!("Illegal datetime format")
+                _ => {
+                    error_message("Can't parse datetime", &self.string_value);
+                    std::process::exit(2);
+                }
             }
         }
 

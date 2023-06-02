8ae09319e49804f219dd4821db692694f5922dd1
Lewis Cook <lcook@FreeBSD.org>
2023-06-02 05:53:35 +0000

    fix build with --no-default-features (#132)
    Mistakingly, both Field::User and Field::Group were not behind
    the `users` feature flag in the parser, causing them to be undefined
    when trying to build without the default features (`users` enabled).

--- src/parser.rs
+++ src/parser.rs
@@ -101,7 +101,9 @@ impl Parser {
                             #[cfg(unix)]
                                 {
                                     fields.push(Expr::field(Field::Mode));
+                                    #[cfg(feature = "users")]
                                     fields.push(Expr::field(Field::User));
+                                    #[cfg(feature = "users")]
                                     fields.push(Expr::field(Field::Group));
                                 }
 

466c2a3aa8debb442aa4c1b02b3c350bf0f011a5
4censord <mail@4censord.de>
2023-11-27 20:49:49 +0100

    Add 'duration' test cases
    This adds testcases for mp3 and mp4 files.
    For mp3, it also adds a new test resource.
    
    This empty (silent) mp3 was created using ffmpeg with the following command:
    ffmpeg -f lavfi -i anullsrc=r=44100:cl=mono -t 35 -q:a 9 -acodec libmp3lame resources/test/audio/silent-35s.mp3

--- src/util/duration/mp3.rs
+++ src/util/duration/mp3.rs
@@ -18,3 +18,33 @@ impl DurationExtractor for Mp3DurationExtractor {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::util::duration::DurationExtractor;
+    use crate::util::duration::Mp3DurationExtractor;
+    use crate::util::Duration;
+    use crate::util::MP3Metadata;
+    use crate::PathBuf;
+    use std::error::Error;
+
+    #[test]
+    fn test_success() -> Result<(), Box<dyn Error>> {
+        let path_string =
+            std::env::var("CARGO_MANIFEST_DIR")? + "/resources/test/" + "audio/silent-35s.mp3";
+        let path = PathBuf::from(path_string);
+
+        let mp3_metadata = |path: PathBuf| -> Option<MP3Metadata> {
+            match mp3_metadata::read_from_file(path) {
+                Ok(mp3_meta) => Some(mp3_meta),
+                _ => None,
+            }
+        }(path.clone());
+
+        assert_eq!(
+            Mp3DurationExtractor.try_read_duration(&path, &mp3_metadata)?,
+            Some(Duration { length: 35 }),
+        );
+        Ok(())
+    }
+}

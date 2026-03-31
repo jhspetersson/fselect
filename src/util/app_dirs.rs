a33d6e9ade53b6a91d87e2045963f7f209de1ed7
jhspetersson <jhspetersson@gmail.com>
2026-03-31 16:24:22 +0200

    fix get_project_dir panic on root config path

--- src/util/app_dirs.rs
+++ src/util/app_dirs.rs
@@ -12,5 +12,16 @@ pub(crate) fn get_project_dir() -> Option<PathBuf> {
 #[cfg(windows)]
 pub(crate) fn get_project_dir() -> Option<PathBuf> {
     ProjectDirs::from("", ORGANIZATION, APPLICATION)
-        .map(|pd| pd.config_dir().parent().unwrap().to_path_buf())
+        .and_then(|pd| pd.config_dir().parent().map(|p| p.to_path_buf()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_project_dir_does_not_panic() {
+        // Should return Some on most systems, but must never panic
+        let _ = get_project_dir();
+    }
 }

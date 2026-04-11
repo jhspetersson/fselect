f28980a249aaf34116b21a28f705ab18a6c91d10
jhspetersson <jhspetersson@gmail.com>
2026-04-12 01:40:04 +0200

    add Playground

---
+++ vendor/wasi-shim/index.js
@@ -0,0 +1,1 @@
+import WASI,{WASIProcExit}from"./wasi.js";export{WASI,WASIProcExit};export{Fd,Inode}from"./fd.js";export{File,Directory,OpenFile,OpenDirectory,PreopenDirectory,ConsoleStdout}from"./fs_mem.js";export{SyncOPFSFile,OpenSyncOPFSFile}from"./fs_opfs.js";export{strace}from"./strace.js";export*as wasi from"./wasi_defs.js";
---
+++ vendor/wasi-shim/strace.js
@@ -0,0 +1,1 @@
+export function strace(imports,no_trace){return new Proxy(imports,{get(target,prop,receiver){const f=Reflect.get(target,prop,receiver);if(no_trace.includes(prop)){return f}return function(...args){console.log(prop,"(",...args,")");const result=Reflect.apply(f,receiver,args);console.log(" =",result);return result}}})}
